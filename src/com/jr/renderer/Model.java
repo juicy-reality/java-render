196837265c7329932f69b8249e6d3e4a0cf88353
denys <denys.potapov@gmail.com>
2018-08-12 15:21:25 +0300

    load new modelk

--- src/com/jr/renderer/Model.java
+++ src/com/jr/renderer/Model.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
-/**
+/**git com
  * Created by denys on 11.08.18.
  */
 public class Model {
@@ -23,7 +23,8 @@ public class Model {
         public Color color;
 
         public Vertex(VectorF location, Color color){
-
+            this.location = location;
+            this.color = color;
         }
     }
 
@@ -43,32 +44,35 @@ public class Model {
         return null;
     }
 
-    public FAce vertex
     public void loadFaces(BufferedImage color, BufferedImage depth){
         int w = color.getWidth();
         int h = color.getHeight();
 
-        // get vertices
-        final Vertex[] vertices = new Vertex[h * w];
-        for (int i = 1; i < w; i++) {
-            for (int j = 1; j <= h; j++) {
+        // get vectors
+        final VectorF[] vectors = new VectorF[h * w];
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j <= h; j++) {
                 float x = (float) i * 2 / w - 1;
                 float y = (float) i * 2 / h - 1;
-                Vertex v = new Vertex();
-                vet
-                vertices[i * w + j] = new Vertex()
+                vectors[i * w + j] = new VectorF(x, y, 0.0f);
             }
         }
 
-        for (int x = 1; x < w; x++) {
-            for (int y = 1; y <= h; y++) {
-                Vertex vertex = new Vertex();
-                vertex.location = vertexes.get(scanner.nextInt() - 1);
-                vertex.coloruv = uvs.get(scanner.nextInt() - 1);
-                vertex.normal = normals.get(scanner.nextInt() - 1);
-                face[i] = vertex;
+        for (int x = 0; x < w - 1; x++) {
+            for (int y = 0; y < h - 1; y++) {
+                final Vertex[] faceRight = new Vertex[3];
+                Color c = new Color(color.getRGB(x, y));
+                faceRight[0] = new Vertex(vectors[y * w + x], c);
+                faceRight[1] = new Vertex(vectors[y * w + x + 1], c);
+                faceRight[2] = new Vertex(vectors[(y + 1) * w + x], c);
+                faces.add(faceRight);
+
+                final Vertex[] faceLeft = new Vertex[3];
+                faceLeft[0] = new Vertex(vectors[y * w + x + 1], c);
+                faceLeft[1] = new Vertex(vectors[(y + 1) * w + x], c);
+                faceLeft[2] = new Vertex(vectors[(y + 1) * w + x + 1], c);
+                faces.add(faceLeft);
             }
         }
-        return new Dimension(img.getWidth(null), img.getHeight(null));
     }
 }
